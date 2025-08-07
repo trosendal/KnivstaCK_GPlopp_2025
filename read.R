0f54aa9551633132966b6eeec3e67664310e5e41
Thomas Rosendal <thomas.rosendal@sva.se>
2025-08-07 19:31:18 +0200

    clean up and add rankings

--- read.R
+++ read.R
@@ -1,5 +1,9 @@
-pdf("A.pdf", width = 24, height = 24)
-par(mar = c(5.1, 7, 4.1, 2.1))
+library(readxl)
+
+## Read the startlist
+startlist <- readxl::read_xlsx("startlist.xlsx")
+
+## Read the results and som manual entries
 df <- read.table("20250806-212445-0212BD-SF.txt")
 df2 <- read.table("extradata.txt")
 names(df) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
@@ -7,20 +11,44 @@ names(df2) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
 df$type <- "chip_read"
 df2$type <- "manual"
 df <- rbind(df, df2)
-df$type <- c("black", "grey")[as.numeric(as.factor(df$type))]
+df$type <- c("black", "grey10")[as.numeric(as.factor(df$type))]
+df$pch <- c(20, 4)[as.numeric(as.factor(df$type))]
 df$datetime <- as.POSIXlt(paste(df$date, df$time), format = "%Y-%m-%d %H:%M:%OS")
+
+## Add the name and class to the results
+df$name <- startlist$Namn[match(df$chip, startlist$ChipNr)]
+df$class <- startlist$Klass[match(df$chip, startlist$ChipNr)]
+
+## limit to just today
+df <- df[df$datetime >= as.POSIXlt("2025-08-06 18:50:00.000", format = "%Y-%m-%d %H:%M:%OS"), ]
+
+## Check for anyone with no Chip times:
+startlist[!startlist$ChipNr %in% df$chip, ]
+## We know that Henrik Holm started and was first in B
+
+
+## Graph the results of A
+pdf("A.pdf", width = 48, height = 24)
+par(mar = c(5.1, 20, 4.1, 2.1))
 cutoff1 <- as.POSIXlt("2025-08-06 19:03:00.000", format = "%Y-%m-%d %H:%M:%OS")
 cutoff2 <- as.POSIXlt("2025-08-06 19:43:00.000", format = "%Y-%m-%d %H:%M:%OS")
 df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2, ]
-df2 <- df2[order(df2$datetime), ]
+df2 <- df2[order(df2$chip, df2$datetime), ]
 df2$chip <- as.factor(df2$chip)
+##
+## Add the lap number
+df2$lap <- as.numeric(ave(df2$datetime, df2$chip, FUN = rank))
+df2$placing <- as.numeric(ave(df2$datetime, df2$lap, FUN = rank))
+##
 plot(y = df2$chip, x = df2$datetime,
-     type = "p", pch = 20, yaxt = "n",
+     type = "p", pch = df2$pch, yaxt = "n",
      ylab = "", xlab = "", xaxt = "n",
-     main = "A grupp", col = df2$type, cex = 2)
+     main = "A grupp", col = df2$type, cex = 3)
 labsy <- levels(df2$chip)
+name <- startlist$Namn[match(labsy, startlist$ChipNr)]
+class <- startlist$Klass[match(labsy, startlist$ChipNr)]
 axis(side = 2, at = sort(as.numeric(unique(df2$chip))),
-     labels = labsy, las = 1)
+     labels = paste0(name, "(", class, ")", ", ", labsy), las = 1)
 labsx <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 41)
@@ -29,32 +57,37 @@ times <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 41)
 axis(side = 1, at = times, las = 2, labels = labsx)
-abline(v = times)
+abline(v = times, col = "grey90")
+## Add lap number and rank
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) + 0.2,
+     labels = paste0("varv: ", df2$lap))
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) - 0.2,
+     labels = paste0("plats: ", df2$placing))
 dev.off()
 
-pdf("C_ungdom_damer.pdf", width = 24, height = 24)
-par(mar = c(5.1, 7, 4.1, 2.1))
-df <- read.table("20250806-212445-0212BD-SF.txt")
-df2 <- read.table("extradata.txt")
-names(df) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
-names(df2) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
-df$type <- "chip_read"
-df2$type <- "manual"
-df <- rbind(df, df2)
-df$type <- c("black", "grey")[as.numeric(as.factor(df$type))]
-df$datetime <- as.POSIXlt(paste(df$date, df$time), format = "%Y-%m-%d %H:%M:%OS")
+pdf("Damer.pdf", width = 48, height = 24)
+par(mar = c(5.1, 20, 4.1, 2.1))
 cutoff1 <- as.POSIXlt("2025-08-06 19:47:00.000", format = "%Y-%m-%d %H:%M:%OS")
 cutoff2 <- as.POSIXlt("2025-08-06 20:27:00.000", format = "%Y-%m-%d %H:%M:%OS")
-df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2, ]
-df2 <- df2[order(df2$datetime), ]
+df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2 & df$class == "Damer", ]
+df2 <- df2[order(df2$chip, df2$datetime), ]
 df2$chip <- as.factor(df2$chip)
+##
+## Add the lap number
+df2$lap <- as.numeric(ave(df2$datetime, df2$chip, FUN = rank))
+df2$placing <- as.numeric(ave(df2$datetime, df2$lap, FUN = rank))
+##
 plot(y = df2$chip, x = df2$datetime,
-     type = "p", pch = 20, yaxt = "n",
+     type = "p", pch = df2$pch, yaxt = "n",
      ylab = "", xlab = "", xaxt = "n",
-     main = "C, ungdom och damer", col = df2$type, cex = 2)
+     main = "Damer", col = df2$type, cex = 3)
 labsy <- levels(df2$chip)
+name <- startlist$Namn[match(labsy, startlist$ChipNr)]
+class <- startlist$Klass[match(labsy, startlist$ChipNr)]
 axis(side = 2, at = sort(as.numeric(unique(df2$chip))),
-     labels = labsy, las = 1)
+     labels = paste0(name, "(", class, ")", ", ", labsy), las = 1)
 labsx <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 41)
@@ -63,32 +96,115 @@ times <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 41)
 axis(side = 1, at = times, las = 2, labels = labsx)
-abline(v = times)
+abline(v = times, col = "grey90")
+## Add lap number and rank
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) + 0.1,
+     labels = paste0("varv: ", df2$lap))
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) - 0.1,
+     labels = paste0("plats: ", df2$placing))
 dev.off()
 
-pdf("B.pdf", width = 24, height = 24)
-par(mar = c(5.1, 7, 4.1, 2.1))
-df <- read.table("20250806-212445-0212BD-SF.txt")
-df2 <- read.table("extradata.txt")
-names(df) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
-names(df2) <- c("pass", "chip", "info", "date", "time", "info1", "info2")
-df$type <- "chip_read"
-df2$type <- "manual"
-df <- rbind(df, df2)
-df$type <- c("black", "grey")[as.numeric(as.factor(df$type))]
-df$datetime <- as.POSIXlt(paste(df$date, df$time), format = "%Y-%m-%d %H:%M:%OS")
+pdf("Ungdomar.pdf", width = 48, height = 12)
+par(mar = c(5.1, 20, 4.1, 2.1))
+cutoff1 <- as.POSIXlt("2025-08-06 19:47:00.000", format = "%Y-%m-%d %H:%M:%OS")
+cutoff2 <- as.POSIXlt("2025-08-06 20:27:00.000", format = "%Y-%m-%d %H:%M:%OS")
+df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2 & df$class == "Ungdom", ]
+df2 <- df2[order(df2$chip, df2$datetime), ]
+df2$chip <- as.factor(df2$chip)
+##
+## Add the lap number
+df2$lap <- as.numeric(ave(df2$datetime, df2$chip, FUN = rank))
+df2$placing <- as.numeric(ave(df2$datetime, df2$lap, FUN = rank))
+##
+plot(y = df2$chip, x = df2$datetime,
+     type = "p", pch = df2$pch, yaxt = "n",
+     ylab = "", xlab = "", xaxt = "n",
+     main = "Ungdom", col = df2$type, cex = 3)
+labsy <- levels(df2$chip)
+name <- startlist$Namn[match(labsy, startlist$ChipNr)]
+class <- startlist$Klass[match(labsy, startlist$ChipNr)]
+axis(side = 2, at = sort(as.numeric(unique(df2$chip))),
+     labels = paste0(name, "(", class, ")", ", ", labsy), las = 1)
+labsx <- seq(from = cutoff1,
+             to = cutoff2,
+             length.out = 41)
+labsx <- format(as.POSIXlt(labsx), "%H:%M:%S")
+times <- seq(from = cutoff1,
+             to = cutoff2,
+             length.out = 41)
+axis(side = 1, at = times, las = 2, labels = labsx)
+abline(v = times, col = "grey90")
+## Add lap number and rank
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) + 0.05,
+     labels = paste0("varv: ", df2$lap))
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) - 0.05,
+     labels = paste0("plats: ", df2$placing))
+dev.off()
+
+pdf("C.pdf", width = 48, height = 24)
+par(mar = c(5.1, 20, 4.1, 2.1))
+cutoff1 <- as.POSIXlt("2025-08-06 19:47:00.000", format = "%Y-%m-%d %H:%M:%OS")
+cutoff2 <- as.POSIXlt("2025-08-06 20:27:00.000", format = "%Y-%m-%d %H:%M:%OS")
+df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2 & df$class == "C", ]
+df2 <- df2[order(df2$chip, df2$datetime), ]
+df2$chip <- as.factor(df2$chip)
+##
+## Add the lap number
+df2$lap <- as.numeric(ave(df2$datetime, df2$chip, FUN = rank))
+df2$placing <- as.numeric(ave(df2$datetime, df2$lap, FUN = rank))
+##
+plot(y = df2$chip, x = df2$datetime,
+     type = "p", pch = df2$pch, yaxt = "n",
+     ylab = "", xlab = "", xaxt = "n",
+     main = "C", col = df2$type, cex = 3)
+labsy <- levels(df2$chip)
+name <- startlist$Namn[match(labsy, startlist$ChipNr)]
+class <- startlist$Klass[match(labsy, startlist$ChipNr)]
+axis(side = 2, at = sort(as.numeric(unique(df2$chip))),
+     labels = paste0(name, "(", class, ")", ", ", labsy), las = 1)
+labsx <- seq(from = cutoff1,
+             to = cutoff2,
+             length.out = 41)
+labsx <- format(as.POSIXlt(labsx), "%H:%M:%S")
+times <- seq(from = cutoff1,
+             to = cutoff2,
+             length.out = 41)
+axis(side = 1, at = times, las = 2, labels = labsx)
+abline(v = times, col = "grey90")
+## Add lap number and rank
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) + 0.2,
+     labels = paste0("varv: ", df2$lap))
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) - 0.2,
+     labels = paste0("plats: ", df2$placing))
+dev.off()
+
+pdf("B.pdf", width = 48, height = 24)
+par(mar = c(5.1, 20, 4.1, 2.1))
 cutoff1 <- as.POSIXlt("2025-08-06 20:32:00.000", format = "%Y-%m-%d %H:%M:%OS")
 cutoff2 <- as.POSIXlt("2025-08-06 21:14:00.000", format = "%Y-%m-%d %H:%M:%OS")
-df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2, ]
-df2 <- df2[order(df2$datetime), ]
+df2 <- df[df$datetime > cutoff1 & df$datetime < cutoff2 & df$class == "B", ]
+df2 <- df2[order(df2$chip, df2$datetime), ]
 df2$chip <- as.factor(df2$chip)
+##
+## Add the lap number
+df2$lap <- as.numeric(ave(df2$datetime, df2$chip, FUN = rank))
+df2$placing <- as.numeric(ave(df2$datetime, df2$lap, FUN = rank))
+##
 plot(y = df2$chip, x = df2$datetime,
-     type = "p", pch = 20, yaxt = "n",
+     type = "p", pch = df2$pch, yaxt = "n",
      ylab = "", xlab = "", xaxt = "n",
-     main = "B gruppen", col = df2$type, cex = 2)
+     main = "B", col = df2$type, cex = 3)
 labsy <- levels(df2$chip)
+name <- startlist$Namn[match(labsy, startlist$ChipNr)]
+class <- startlist$Klass[match(labsy, startlist$ChipNr)]
 axis(side = 2, at = sort(as.numeric(unique(df2$chip))),
-     labels = labsy, las = 1)
+     labels = paste0(name, "(", class, ")", ", ", labsy), las = 1)
 labsx <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 43)
@@ -97,5 +213,12 @@ times <- seq(from = cutoff1,
              to = cutoff2,
              length.out = 43)
 axis(side = 1, at = times, las = 2, labels = labsx)
-abline(v = times)
+abline(v = times, col = "grey90")
+## Add lap number and rank
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) + 0.2,
+     labels = paste0("varv: ", df2$lap))
+text(x = df2$datetime,
+     y = as.numeric(df2$chip) - 0.2,
+     labels = paste0("plats: ", df2$placing))
 dev.off()
